bc176dd9d111c94074a66ef43eabf73de804d058
markohranic <markohr9@gmail.com>
2022-03-05 00:11:28 +0100

    Brojevi2 solution

---
+++ Brojevi2/Brojevi2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brojevi2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, C;
+            int suma = 0;
+            var rezultat = new List<int>();
+            Console.Write("A = ");
+            A = int.Parse(Console.ReadLine());
+            Console.Write("B = ");
+            B = int.Parse(Console.ReadLine());
+            Console.Write("C = ");
+            C = int.Parse(Console.ReadLine());
+
+            // swap
+            if (B<A)
+            {
+                A = A + B;
+                B = A - B;
+                A = A - B;
+            }
+
+            for(int i = A; i <= B; i++)
+            {
+                if (i % C == 0)
+                {
+                    rezultat.Add(i);
+                }
+            }
+            
+            if(rezultat.Count != 0)
+            {
+                Console.Write("Rezultat je: ");
+                foreach (int broj in rezultat)
+                {
+                    Console.Write(broj + " ");
+                    suma += broj;
+                }
+                Console.WriteLine();  
+                Console.WriteLine("Suma je: " + suma);
+            } 
+            else
+            {
+                Console.WriteLine("Nema brojeva između "+A+" i "+B+" koji su dijeljivi sa "+C);
+            }
+            Console.ReadLine();
+        }
+    }
+}
