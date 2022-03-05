a5860dd53e8a927211007c3ebc2a5e0b029c6f5f
markohranic <markohr9@gmail.com>
2022-03-05 03:52:02 +0100

    -

---
+++ NogometneUtakmice/NogometneUtakmice/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NogometneUtakmice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] rezultati = new string[]
+            {
+                "HRV 2:0 NIG",
+                "HRV 3:0 ARG",
+                "HRV 2:1 ISL",
+                "HRV 1:1 DAN",
+                "HRV 2:2 RUS",
+                "HRV 2:1 ENG",
+                "HRV 2:4 FRA"
+            };
+            int pobjeda = 0;
+            int poraz = 0;
+            int remi = 0;
+            foreach(string rezultat in rezultati)
+            {
+                string[] trenutniRezultat = rezultat.Split(' ');
+                string[] brojevi = trenutniRezultat[1].Split(':');
+                
+                if(brojevi[0]== brojevi[1])
+                {
+                    remi++;
+                }
+                else if(int.Parse(brojevi[0]) > int.Parse(brojevi[1]))
+                {
+                    pobjeda++;
+                }
+                else if (int.Parse(brojevi[0]) < int.Parse(brojevi[1]))
+                {
+                    poraz++;
+                }
+
+
+            }
+
+            Console.WriteLine("Ukupno ostvarenih pobjeda: " + pobjeda);
+            Console.WriteLine("Ukupno ostvarenih poraza: "+ poraz);
+            Console.WriteLine("Ukupno ostvarenih remija: "+remi);
+            Console.Read();
+        }
+    }
+}
---
+++ NogometneUtakmice2/NogometneUtakmice2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NogometneUtakmice2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] rezultati = new string[]
+            {
+                "ARG 1:1 ISL",
+                "HRV 2:0 NIG",
+                "ARG 0:3 HRV",
+                "NIG 2:0 ISL",
+                "NIG 1:2 ARG",
+                "ISL 1:2 HRV"
+            };
+
+            Console.WriteLine("TABLICA REZULTATA");
+            Console.WriteLine("Upišite oznaku reprezentacije (HRV, ARG, NIG ili ISL).");
+            string drzava = Console.ReadLine();
+            Console.WriteLine("Utakmice reprezentacije: " + drzava);
+
+            for(int i = 0; i < rezultati.Length; i++)
+            {
+                string[] rasclanjenRezultat = rezultati[i].Split(' ');
+                if(rasclanjenRezultat[0] == drzava)
+                {
+                    Console.WriteLine(rezultati[i]);
+                }
+                else if(rasclanjenRezultat[2] == drzava)
+                {
+                    EditString(rasclanjenRezultat);
+                }
+            }
+            Console.ReadLine();
+        }
+        static void EditString(string[] rasclanjenRezultat)
+        {
+            string invertedMiddlePart = string.Empty;
+            for(int i = rasclanjenRezultat[1].Length - 1; i >= 0; i--)
+            {
+                invertedMiddlePart += rasclanjenRezultat[1][i];
+            }
+
+            string editedString = rasclanjenRezultat[2] + " " + invertedMiddlePart + " " + rasclanjenRezultat[0];
+            Console.WriteLine(editedString);
+            return;
+        }
+    }
+}
